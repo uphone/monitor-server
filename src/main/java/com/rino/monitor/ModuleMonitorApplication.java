0aef14573a8128f00b35bf177046ba57cce8cbd8
uphone <595228808@qq.com>
2021-01-27 13:26:54 +0800

    init

---
+++ src/main/java/com/rino/monitor/ModuleMonitorApplication.java
@@ -0,0 +1,13 @@
+package com.rino.monitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModuleMonitorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ModuleMonitorApplication.class, args);
+    }
+
+}
