6e05eeaca757ac23625af1e813ec1152d19d7cdb
uphone <595228808@qq.com>
2021-02-01 14:06:20 +0800

    optimize

---
+++ src/main/java/com/rino/monitor/web/WebConfig.java
@@ -0,0 +1,21 @@
+package com.rino.monitor.web;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@Slf4j
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowCredentials(true)
+//                .allowedHeaders("GET", "HEAD", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*") // 注意： 这里需要设置为*，在Session拦截器中需要设置为特定的headers
+                .allowedOrigins("*")
+                .allowedMethods("*");
+    }
+}
